4ddea721e02fc8c2733acb78cf15eacfc5f69038
Hakim128 <61293071+Hakim128@users.noreply.github.com>
2020-03-16 13:07:59 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("688745243809677322")
+setInterval(function() {
+channel.send(`ما طاح حيلليّ ولا ضاقتَ بي ضلوعي
+‏بحاول انسٌى واعيش اليوم مع بكره `);
+}, 20)
+})
+
+client.login(process.env.BOT_TOKEN);
